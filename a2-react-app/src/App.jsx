49ee5cb17347bea9725407c0ee3e555cf171b679
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-03-23 18:45:51 -0600

    testing

--- a2-react-app/src/App.jsx
+++ a2-react-app/src/App.jsx
@@ -4,7 +4,7 @@ import { createClient} from '@supabase/supabase-js';
 const supaUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supaUrl, supabaseAnonKey);
-
+//why u no work?
 
 function App() {
   const [seasons, setSeasons] = useState([]);
