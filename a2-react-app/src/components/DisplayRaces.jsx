90625a04bb86d0ca0f2cf392dd4a4f98bf4a0341
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-04-09 03:38:56 -0600

    debugging image

--- a2-react-app/src/components/DisplayRaces.jsx
+++ a2-react-app/src/components/DisplayRaces.jsx
@@ -3,32 +3,32 @@ import { Link } from "react-router-dom";
 const DisplayRaces = (props) => {
   return (
     <div className="relative min-h-screen bg-center bg-cover flex items-center justify-center" style={{ backgroundImage: `url('/sara-ruffoni-MjVdmqBCoXg-unsplash.jpg')` }}>
-    <section className="font-sans mx-auto w-3/4">
+    <section className="font-sans mx-auto w-1/2">
       {props.selectedSeason && (
-        <h2 className="text-center text-lg font-normal tracking-wide">
+        <h2 className="text-center text-5xl font-sans font-bold tracking-wide text-white mb-3">
           {props.selectedSeason} Races
         </h2>
       )}
-      <div className="divide-y divide-gray-200">
-        <div className="bg-gray-50 flex justify-between px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-          <div>Round</div>
-          <div>Circuit</div>
-          <div>Actions</div>
+      <div className="divide-y divide-gray-200 ">
+        <div className="bg-gray-300 flex justify-between px-6 py-3 text-left text-xs py-4 font-medium text-gray-500 uppercase tracking-wider bg-opacity-60">
+          <div className="text-white font-sans font-semibold">Round</div>
+          <div className="text-white font-sans font-semibold">Circuit</div>
+          <div className="text-white font-sans font-semibold">Actions</div>
         </div>
-        <div className="bg-white">
+        <div className="bg-white bg-opacity-60">
           {props.seasonRaces &&
             props.seasonRaces.map((race, indx) => (
-              <div key={indx} className={`flex justify-between px-6 py-3 ${indx % 2 !== 0 ? 'bg-[#F8F8F8]' : ''}`}>
+              <div key={indx} className={`flex justify-between px-6 py-2 ${indx % 2 !== 0 ? 'bg-[#F8F8F8]' : ''}`}>
                 <div className="whitespace-nowrap">{race.round}</div>
                 <div className="whitespace-nowrap">{race.name}</div>
                 <div className="whitespace-nowrap">
                   <Link to={`/race-results/${race.raceId}`} className="mr-2">
-                    <button className="text-white bg-customRed1 hover:bg-[#6b7075] hover:text-white font-medium rounded text-sm px-3 py-2 text-center inline-flex items-center">
+                    <button className="text-white bg-[#A9A9A9] hover:bg-[#6b7075] hover:text-white font-medium rounded text-sm px-3 py-2 text-center inline-flex items-center">
                       Results
                     </button>
                   </Link>
                   <Link to={`/race-standings/${race.raceId}`} className="mr-2">
-                    <button className="text-white bg-customRed1 hover:bg-[#6b7075] hover:text-white font-medium rounded text-sm px-3 py-2 text-center inline-flex items-center">
+                    <button className="text-white bg-[#A9A9A9] hover:bg-[#6b7075] hover:text-white font-medium rounded text-sm px-3 py-2 text-center inline-flex items-center">
                       Standings
                     </button>
                   </Link>
